cb8e1dc4ef7bafd505a19b638950a61e0297b38f
Mike <mfriedmanisgod@netscape.net>
2015-09-09 12:20:45 -0400

    TEST

--- StopWatch.Winform/Form1.cs
+++ StopWatch.Winform/Form1.cs
@@ -76,7 +76,7 @@ namespace StopWatch.Winform
         private void DisplayTimer(Label timerDisplay)
         {
             timerDisplay.Text = hour + ":" + min + ":" + sec + ":" + ms.ToString();
-            //test
+            //TEST
         }
 
     }
