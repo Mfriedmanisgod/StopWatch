6bc0ad69bbfaa7ed29afe70c1a4d79a5d7679ffa
Mike <mfriedmanisgod@netscape.net>
2015-09-09 12:08:05 -0400

    First Commit - Completed project

---
+++ StopWatch.Winform/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch.Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hour, min, sec, ms = 0;
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DisplayTimer(label1);
+            ms++;
+             
+            if (ms == 10)
+            {
+                sec++;
+                ms = 0;
+            }
+                
+            if (sec == 60)
+            {
+                min++;
+                sec = 0;
+            }
+            
+            if (min == 60)
+            {
+                hour++;
+                min = 0;
+            }
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ms = 0;
+            sec = 0;
+            min = 0;
+            hour = 0;
+            DisplayTimer(label1);
+            
+        }
+
+        private void DisplayTimer(Label timerDisplay)
+        {
+            timerDisplay.Text = hour + ":" + min + ":" + sec + ":" + ms.ToString();
+        }
+
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class StopWatchapp
+    {
+        public void Run()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to STERPWARTCH!!!!");
+            Console.WriteLine();
+            Console.WriteLine("Please enter one of the following commands:");
+            Console.WriteLine();
+            Displaycommands();
+
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew(); // = new system.diagnostics.stopwatch stopwatch = new system.diagnostics.stopwatch?????
+            stopwatch.Reset();
+            
+            bool Running = true;
+            
+            while(Running)
+            {
+                string userCommands = Console.ReadLine();
+                userCommands = userCommands.ToLower(); 
+
+                if (userCommands == "start")
+                {
+                    stopwatch.Start();
+                    Console.WriteLine("The timer is rolling...type,'stop' to clock your time...");
+                    Console.WriteLine();
+                    Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                }
+                
+                else if (userCommands == "stop")
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                }
+                
+                else if (userCommands == "reset")
+                {
+                    stopwatch.Reset();
+                    Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                }
+                
+                else if (userCommands == "current time")
+                {
+                    Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                }
+                
+                else
+                {
+                    Console.WriteLine("Sorry, that command is not supported, Please enter one of the following:");
+                    Console.WriteLine();
+                    Displaycommands();
+                }
+
+             }
+            
+        }
+
+        private void Displaycommands()
+        {
+            Console.WriteLine("START");
+            Console.WriteLine();
+            Console.WriteLine("STOP");
+            Console.WriteLine();
+            Console.WriteLine("RESET");
+            Console.WriteLine();
+            Console.WriteLine("CURRENT TIME");
+            Console.WriteLine();
+        }
+
+    }
+}
